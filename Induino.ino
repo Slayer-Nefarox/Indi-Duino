3ac0a102b8d8e33175ea69caf2db2c9511d73132
Gabriel Spressola Ziviani <gabrielziviani@gmail.com>
2026-02-16 15:51:54 -0300

    Refactor Induino code for Arduino Nano compatibility

--- Induino.ino
+++ Induino.ino
@@ -1,16 +1,14 @@
 //ligar os seguintes pinos:
-//TCS3472 ----------- Arduino Uno R3
+//TCS3472 ----------- Arduino Nano
 //VIn --------------- 5v
 //Gnd --------------- Gnd
 //Scl --------------- A5
 //Sda --------------- A4
-//Led --------------- 2
+//Led --------------- d5 (Pwm)
 
-//Ponte H ----------- Arduino Uno R3
-//in1 --------------- 11
-//in2 --------------- 10
-//in3 --------------- 6
-//in4 --------------- 5
+//Ponte H ----------- Arduino Nano
+//in1 --------------- d2
+//in2 --------------- d3
 
 //Adicionar Biblioteca "Adafruit_TCS34725.h" ao Arduino IDE 2.0
 //Possivel encontrar em https://github.com/adafruit/Adafruit_TCS34725
@@ -19,200 +17,148 @@
 
 #include <Wire.h>
 #include "Adafruit_TCS34725.h"
+#include <Servo.h> 
 
-#define Mot_L_Frente 11
-#define Mot_L_Tras 10
-#define Mot_R_Frente 6
-#define Mot_R_Tras 5
+// --- Configuração ---
+#define PIN_SERVO_ESQ 2
+#define PIN_SERVO_DIR 3
 
-#define Tempo 1500 // tempo (em milissegundos) entre comandos
+#define LedEnable 5
 
-#define intervalo 15 // intervalo de tolerancia da faixa de cor
+// --- Calibração Servos ---
+#define PARADO 90 
+#define MAX_FRENTE_ESQ 135 
+#define MAX_TRAS_ESQ 45     
+#define MAX_FRENTE_DIR 45 
+#define MAX_TRAS_DIR 135   
 
-#define LedEnable 2 //pino que liga/desliga o led (caso use iluminação propria da placa)
+#define INTERVALO 15
 
-Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_60X);// ganho possivel: X01, X04, X16, X60.
+// Flags de Estado
+bool detectouCor = false; 
 
-//variaveis globais
-  //Velocidades individuais para cada motor para balancear imparidade de hardware. Minimo 1, máximo até 63.
-int speed_L = 59;
-int speed_R = 63;
+Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_60X);
+Servo servoEsq;
+Servo servoDir;
 
-int speed_mult = 0;
-int ultima_Cor = 0;
+// Cores alvo (R, G, B)
+int verde[3] = {55, 122, 75};
+int vermelho[3] = {179, 48, 42};
+int azul[3] = {23, 90, 143};
+int roxo[3] = {58, 75, 128};
+int amarelo[3] = {111, 104, 59};
 
-//indice de cores
-int verde[3] = {60,110,80};
-int vermelho[3] = {150, 55,55};
-int azul[3] = {28,86,131};
-int roxo[3] = {103,51,94};
-// ...
-
-void debug(float red, float green, float blue)
-{
-  //debug valor do RGB
-  Serial.print("R:\t"); Serial.print(int(red)); 
-  Serial.print("\tG:\t"); Serial.print(int(green)); 
-  Serial.print("\tB:\t"); Serial.print(int(blue));
-  Serial.print("\n");
+void parar() {
+  servoEsq.write(PARADO);
+  servoDir.write(PARADO);
 }
 
-bool faixa(int var, int valor)
-{
-  if( var > valor-intervalo && var <= valor+intervalo )// se 'var' estiver dentro do intervalo levando em conta a margem de tolerância
-  {
-    return true;
-  }
-  else
-  {
-    return false;
-  }
+void moverFrente() {
+  servoEsq.write(MAX_FRENTE_ESQ);
+  servoDir.write(MAX_FRENTE_DIR);
 }
 
-void setup() 
-{
+// CORREÇÃO: Removemos moverFrente() daqui para permitir controle temporal no loop
+void virarDireita() {
+  servoEsq.write(MAX_FRENTE_ESQ);
+  servoDir.write(PARADO); 
+}
+
+void virarEsquerda() {
+  servoDir.write(MAX_FRENTE_DIR);
+  servoEsq.write(PARADO);
+}
+
+void comemora() {
+  servoDir.write(MAX_FRENTE_DIR);
+  servoEsq.write(MAX_TRAS_ESQ);
+}
+
+bool verificaCor(float r, float g, float b, int alvo[]) {
+  return (r > alvo[0] - INTERVALO && r < alvo[0] + INTERVALO) &&
+         (g > alvo[1] - INTERVALO && g < alvo[1] + INTERVALO) &&
+         (b > alvo[2] - INTERVALO && b < alvo[2] + INTERVALO);
+}
+
+void setup() {
   Serial.begin(9600);
-  pinMode(Mot_L_Frente, OUTPUT);
-  pinMode(Mot_L_Tras, OUTPUT);
-  pinMode(Mot_R_Frente, OUTPUT);
-  pinMode(Mot_R_Tras, OUTPUT);
-
-  analogWrite(Mot_L_Frente, 0);
-  analogWrite(Mot_L_Tras, 0);
-
-  analogWrite(Mot_R_Frente, 0);
-  analogWrite(Mot_R_Tras, 0);
+  servoEsq.attach(PIN_SERVO_ESQ);
+  servoDir.attach(PIN_SERVO_DIR);
+  parar(); 
   
-  if (tcs.begin()) 
-  {
-    Serial.println("Found sensor");
-  } 
-  else 
-  {
-    while (1) // ERRO
-    {
-      Serial.println("No TCS34725 found ... check your connections and reboot");
-    }
-  }
-
   pinMode(LedEnable, OUTPUT);
+  digitalWrite(LedEnable, HIGH); // Liga LED para calibração inicial
+
+  if (tcs.begin()) {
+    Serial.println("Sensor encontrado");
+  } else {
+    Serial.println("TCS34725 nao encontrado. Travando.");
+    while (1);
+  }
 }
 
-void loop() 
-{
-  digitalWrite(LedEnable, HIGH);// controle do LED
-  float tempo_delay = Tempo;//estando mais rápido, precisa de menos tempo pra sair do cartão
-  float red, green, blue;
-  tcs.getRGB(&red, &green, &blue);
+void loop() {
+  // CORREÇÃO: LED ligado para leitura
+  digitalWrite(LedEnable, HIGH);
+  
+  float r, g, b;
+  tcs.getRGB(&r, &g, &b);
 
-  if(faixa( red, verde[0] ) && faixa(green, verde[1]) && faixa( blue, verde[2]) ) //1: verde
-  { 
-    if(ultima_Cor != 1)
-    {
-      Serial.print("Frente ou acelera.");
-      if(speed_mult<4)
-      {
-        speed_mult +=2;
-        tempo_delay = Tempo / ((2/3)*speed_mult);//toda vez que atualizar a velocidade, atulizar o delay de virada
-      }
-      ultima_Cor = 1;
-      delay(500);
-      analogWrite(Mot_L_Frente, speed_mult*speed_L);
-      analogWrite(Mot_L_Tras, 0); 
-      analogWrite(Mot_R_Frente, speed_mult*speed_R);
-      analogWrite(Mot_R_Tras, 0);
+  // Debug (opcional, remova para performance)
+  // Serial.print("R: "); Serial.print(r); Serial.print(" G: "); Serial.print(g); Serial.print(" B: "); Serial.println(b);
+
+  if (verificaCor(r, g, b, verde)) {
+    if (!detectouCor) {
+      Serial.println("Verde: FRENTE 3s");
+      moverFrente();
+      delay(3000); // Bloqueante: O robô anda 3s sem ler sensores
+      detectouCor = true;
     }
-    else
-    {
-      Serial.print("Frente ou acelera, Negado.");
+  }
+  else if (verificaCor(r, g, b, azul)) {
+    if (!detectouCor) {
+      Serial.println("Azul: VIRAR DIR + FRENTE");
+      virarDireita();
+      delay(800); // CORREÇÃO: Tempo para realizar a curva
+      moverFrente();
+      delay(1000); // Tempo andando reto após a curva
+      detectouCor = true;
     }
+  }
+  else if (verificaCor(r, g, b, vermelho)) {
+    if (!detectouCor) {
+      Serial.println("Vermelho: PARAR 3s");
+      parar();
+      delay(3000);
+      detectouCor = true;
+    }
+  }
+  else if (verificaCor(r, g, b, roxo)) {
+    if (!detectouCor) {
+      Serial.println("Roxo: VIRAR ESQ + FRENTE");
+      virarEsquerda();
+      delay(800); // CORREÇÃO: Tempo para realizar a curva
+      moverFrente();
+      delay(1000);
+      detectouCor = true;
+    }
+  }
+  else if (verificaCor(r, g, b, amarelo)) {
+    if (!detectouCor) {
+      Serial.println("Amarelo: COMEMORA");
+      comemora();
+      delay(1000); // CORREÇÃO: Tempo para executar o giro
+      parar();
+      detectouCor = true;
+    }
+  }
+  else {
+    // CORREÇÃO LÓGICA: Só reseta a flag se estiver vendo "chão" (fora das cores)
+    // Isso evita re-trigger imediato enquanto ainda está passando sobre o cartão
+    Serial.println("Cor desconhecida / Chão");
+    detectouCor = false; 
     
+    // Opcional: Manter movimento anterior ou parar se não ver cor
+    // parar(); 
   }
-  else if(faixa( red, azul[0] ) && faixa(green, azul[1]) && faixa( blue, azul[2]) ) //2: Azul
-  { 
-    if(ultima_Cor != 2)
-    {
-      Serial.print("Virar a direita.");
-      ultima_Cor = 2;
-      delay(500);
-      analogWrite(Mot_L_Frente, speed_mult*speed_L);
-      analogWrite(Mot_L_Tras, 0); 
-      analogWrite(Mot_R_Frente, 0);
-      analogWrite(Mot_R_Tras, 0); 
-      delay(tempo_delay);//ajustar tempo_delay para virar 90 graus 
-      analogWrite(Mot_L_Frente, speed_mult*speed_L);
-      analogWrite(Mot_L_Tras, 0); 
-      analogWrite(Mot_R_Frente, speed_mult*speed_R);
-      analogWrite(Mot_R_Tras, 0);
-    }
-    else
-    {
-      Serial.print("Virar a direita, Negado.");
-    }
-    
-  }
-  else if(faixa( red, vermelho[0] ) && faixa(green, vermelho[1]) && faixa( blue, vermelho[2]) ) //3: vermelho
-  { 
-    if(ultima_Cor != 3)
-    {
-      Serial.print("Desacelera ou para.");
-      if(speed_mult>0)
-      {
-        speed_mult -=2;
-        tempo_delay = Tempo / ((2/3)*speed_mult);//toda vez que atualizar a velocidade, atulizar o delay de virada
-      }
-      ultima_Cor = 3;
-      delay(500);
-      analogWrite(Mot_L_Frente, speed_mult*speed_L);
-      analogWrite(Mot_L_Tras, 0); 
-      analogWrite(Mot_R_Frente, speed_mult*speed_R);
-      analogWrite(Mot_R_Tras, 0);
-    }
-    else
-    {
-      Serial.print("Desacelera ou para, Negado.");
-    }
-    
-  }
-  else if(faixa( red, roxo[0] ) && faixa(green, roxo[1]) && faixa( blue, roxo[2]) ) //4: roxo
-  { 
-    if(ultima_Cor != 4)
-    {
-      Serial.print("Virar a esquerda.");
-      delay(500);
-      ultima_Cor = 4;
-      analogWrite(Mot_L_Frente, 0);
-      analogWrite(Mot_L_Tras, 0);
-      analogWrite(Mot_R_Frente, speed_mult*speed_R);
-      analogWrite(Mot_R_Tras, 0);
-      delay(tempo_delay);//ajustar tempo_delay para virar 90 graus 
-      analogWrite(Mot_L_Frente, speed_mult*speed_L);
-      analogWrite(Mot_L_Tras, 0);
-      analogWrite(Mot_R_Frente, speed_mult*speed_R);
-      analogWrite(Mot_R_Tras, 0);
-    }
-    else
-    {
-      Serial.print("Virar a esquerda, Negado.");
-    }
-  }
-  else //nenhuma cor cadastrada
-  {
-    Serial.print("Nenhuma cor detectada entre as cadastradas.");
-    ultima_Cor = 0;
-  }
-  //modelo de estrutura para adicionar cores
-  /*
-  else if(faixa( red, cor[0] ) && faixa(green, cor[1]) && faixa( blue, cor[2]) ) // cor
-  { 
-    Serial.print("frente");
-    analogWrite(Mot_L_Frente, 200);
-    analogWrite(Mot_L_Tras, 0); 
-    analogWrite(Mot_R_Frente, 200);
-    analogWrite(Mot_R_Tras, 0); 
-    delay(tempo_delay);
-  }
-  */
-  debug(red, green, blue);
 }
-
