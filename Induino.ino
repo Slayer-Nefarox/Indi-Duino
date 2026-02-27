482d1016d9b2ac20a73bbeb461fe3588b49ff2ce
Gabriel Spressola Ziviani <gabrielziviani@gmail.com>
2026-02-26 22:48:22 -0300

    Refactor Induino.ino for clarity and functionality
    Added gyroscope functionality

--- Induino.ino
+++ Induino.ino
@@ -10,21 +10,35 @@
 //in1 --------------- d2
 //in2 --------------- d3
 
+/* gyroscope (BMI160)
+VIN → 5V 
+GND → GND
+SCX → D13 (SCK)
+SDX → D12 (MISO)
+SDA → D11 (MOSI)
+CS → D10 (pino digital configurado como Chip Select)
+*/
+
 //Adicionar Biblioteca "Adafruit_TCS34725.h" ao Arduino IDE 2.0
 //Possivel encontrar em https://github.com/adafruit/Adafruit_TCS34725
-//adicionar via "bibliotecas" na arduino IDE 2.0 também é possivel, e BEM mais viável.
+//Adicionar via "bibliotecas" na arduino IDE 2.0 também é possivel, e BEM mais viável.
+//Bibliotecas Servo.h e SPI.h são padrão no ambiente de execução. Não é necessário adicionar.
 
 
 #include <Wire.h>
 #include "Adafruit_TCS34725.h"
 #include <Servo.h> 
+#include <SPI.h>
+
 
 // --- Configuração ---
 #define PIN_SERVO_ESQ 2
 #define PIN_SERVO_DIR 3
 
-#define LedEnable_integrated 5
-#define LedEnable_dedicated 6
+#define LedEnable1 5
+#define LedEnable2 6
+
+#define PIN_CS 10
 
 // --- Calibração Servos ---
 #define PARADO 90 
@@ -33,13 +47,30 @@
 #define MAX_FRENTE_DIR 20 
 #define MAX_TRAS_DIR 180   
 
-#define INTERVALO 10
+#define INTERVALO 30
+
+// =====================
+// Registradores
+// =====================
+const uint8_t REG_CMD        = 0x7E;
+const uint8_t REG_GYRO_X_L   = 0x0C;
+const uint8_t REG_GYRO_RANGE = 0x43;
+const uint8_t REG_GYRO_CONF  = 0x42;
+
+// =====================
+// Escala
+// =====================
+const float GYRO_SCALE_250DPS = 131.2; // LSB/(°/s) para ±250 dps
+
+// =====================
+// Variáveis
+// =====================
+int16_t rawX, rawY, rawZ;
+float gyroX, gyroY, gyroZ;
 
 
 // Flags de Estado
-bool detectouCor = false; 
-
-bool velocidade = false;
+bool detectou_Cor_Antes = false; 
 
 Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_60X);
 Servo servoEsq;
@@ -52,9 +83,26 @@ int azul[3] = {23, 90, 143};
 int roxo[3] = {58, 75, 128};
 int amarelo[3] = {111, 104, 59};
 
+
+void writeRegister(uint8_t reg, uint8_t data) {
+  digitalWrite(PIN_CS, LOW);
+  SPI.transfer(reg & 0x7F);  // bit7 = 0 → write
+  SPI.transfer(data);
+  digitalWrite(PIN_CS, HIGH);
+}
+
+void readRegisters(uint8_t reg, uint8_t count, uint8_t* buffer) {
+  digitalWrite(PIN_CS, LOW);
+  SPI.transfer(reg | 0x80);  // bit7 = 1 → read
+  for (uint8_t i = 0; i < count; i++) {
+    buffer[i] = SPI.transfer(0x00);
+  }
+  digitalWrite(PIN_CS, HIGH);
+}
+
 void parar() {
-  servoEsq.write(PARADO );
-  servoDir.write(PARADO );
+  servoEsq.write(PARADO);
+  servoDir.write(PARADO);
 }
 
 void moverFrente() {
@@ -78,100 +126,151 @@ void comemora() {
   servoEsq.write(MAX_TRAS_ESQ);
 }
 
-bool verificaCor(float r, float g, float b, int alvo[]) {
+void read_position(uint8_t vetorbuffer[])
+{
+   readRegisters(REG_GYRO_X_L, 6, vetorbuffer);
+
+  rawX = vetorbuffer[1] << 8 | vetorbuffer[0];
+  rawY = vetorbuffer[3] << 8 | vetorbuffer[2];
+  rawZ = vetorbuffer[5] << 8 | vetorbuffer[4];
+
+  gyroX = rawX / GYRO_SCALE_250DPS;
+  gyroY = rawY / GYRO_SCALE_250DPS;
+  gyroZ = rawZ / GYRO_SCALE_250DPS;
+
+  Serial.print("Gx: ");
+  Serial.print(gyroX);
+  Serial.print(" | Gy: ");
+  Serial.print(gyroY);
+  Serial.print(" | Gz: ");
+  Serial.println(gyroZ);
+}
+
+bool verificaCor(float r, float g, float b, int alvo[]) 
+{
   return (r > alvo[0] - INTERVALO && r < alvo[0] + INTERVALO) &&
          (g > alvo[1] - INTERVALO && g < alvo[1] + INTERVALO) &&
          (b > alvo[2] - INTERVALO && b < alvo[2] + INTERVALO);
 }
 
-void setup() {
+void setup() 
+{
   Serial.begin(9600);
   servoEsq.attach(PIN_SERVO_ESQ);
   servoDir.attach(PIN_SERVO_DIR);
   parar(); 
+
+  //gyro-----------------------
+  pinMode(PIN_CS, OUTPUT);
+  digitalWrite(PIN_CS, HIGH);  // Mantém desativado
+
+  SPI.begin();  // Inicializa SPI
+
+  // Configura SPI (modo 0, até 1 MHz seguro)
+  SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE0));
+
+  delay(100);
+
+  // Coloca giroscópio em modo normal
+  writeRegister(REG_CMD, 0x15);  // 0x15 = gyro normal mode
+  delay(100);
+
+  // Configura faixa ±250 dps
+  writeRegister(REG_GYRO_RANGE, 0x00);
+
+  // Configura ODR padrão (100 Hz)
+  writeRegister(REG_GYRO_CONF, 0x28);
+
+  //------------------------------
+  //TCS---------------------------
   
-  pinMode(LedEnable_integrated, OUTPUT);
-  analogWrite(LedEnable_integrated, 200); 
-  analogWrite(LedEnable_dedicated, 90);
+  pinMode(LedEnable1, OUTPUT);
+  analogWrite(LedEnable1, 125); // Liga LED para calibração inicial
+  pinMode(LedEnable2, OUTPUT);
+  analogWrite(LedEnable2, 80); // Liga LED para calibração inicial
+  
+  
   if (tcs.begin()) 
   {
     Serial.println("Sensor encontrado");
-  } else 
+  } 
+  else 
   {
-    Serial.println("TCS34725 nao encontrado. reinicie o robô.");
+    Serial.println("TCS34725 nao encontrado. Travando.");
     while (1);
   }
 }
 
 void loop() 
 {
+
   float r, g, b;
   tcs.getRGB(&r, &g, &b);
 
   // Debug (opcional, remova para performance)
-  Serial.print("R: "); Serial.print(r); Serial.print(" G: "); Serial.print(g); Serial.print(" B: "); Serial.println(b);
+  //Serial.print("R: "); Serial.print(r); Serial.print(" G: "); Serial.print(g); Serial.print(" B: "); Serial.println(b);
+
+  uint8_t buffer[6];
+
+  read_position(buffer);
 
   if (verificaCor(r, g, b, verde)) 
   {
-    if (!detectouCor) 
+    if (!detectou_Cor_Antes) 
     {
-      velocidade = true;
       delay(3000);
       Serial.println("Verde: FRENTE");
       moverFrente();
-      detectouCor = true;
-      
+      detectou_Cor_Antes = true;
     }
   }
   else if (verificaCor(r, g, b, azul)) 
   {
-    if (!detectouCor && velocidade) 
+    if (!detectou_Cor_Antes) 
     {
       Serial.println("Azul: VIRAR DIR + FRENTE");
       virarDireita();
       delay(800); // CORREÇÃO: Tempo para realizar a curva
       moverFrente();
-      detectouCor = true;
+      detectou_Cor_Antes = true;
     }
   }
   else if (verificaCor(r, g, b, vermelho)) 
   {
-    if (!detectouCor && velocidade) 
+    if (!detectou_Cor_Antes) 
     {
-      Serial.println("Vermelho: PARAR 3s");
+      Serial.println("Vermelho: PARAR");
       parar();
       delay(5000);
-      detectouCor = true;
-      velocidade = false;//fim de percurso
+      detectou_Cor_Antes = true;
     }
   }
   else if (verificaCor(r, g, b, roxo)) 
   {
-    if (!detectouCor && velocidade) 
+    if (!detectou_Cor_Antes) 
     {
       Serial.println("Roxo: VIRAR ESQ + FRENTE");
       virarEsquerda();
       delay(800); // CORREÇÃO: Tempo para realizar a curva
       moverFrente();
-      detectouCor = true;
+      detectou_Cor_Antes = true;
     }
   }
   else if (verificaCor(r, g, b, amarelo)) 
   {
-    if (!detectouCor && velocidade) 
+    if (!detectou_Cor_Antes) 
     {
       Serial.println("Amarelo: COMEMORA");
       comemora();
       delay(5000); // CORREÇÃO: Tempo para executar o giro
       parar();
-      detectouCor = true;
-      velocidade = false;//fim de percurso
+      detectou_Cor_Antes = true;
     }
   }
   else 
   {
-    Serial.println("Cor desconhecida / Chão");
-    detectouCor = false; 
+    //Serial.println("Cor desconhecida / Chão");
+    detectou_Cor_Antes = false; 
     
   }
 }
